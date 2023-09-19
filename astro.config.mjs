1066b78e642b2f78a799282611f65222a9449d27
Miguel Atencio Vargas <atencio947@gmail.com>
2023-09-19 19:17:05 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
-import rehypePrettyCode from "rehype-pretty-code";
 
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    syntaxHighlight: false,
-    rehypePlugins: [[rehypePrettyCode, {
-
-    }]],
-  }
+    shikiConfig: {
+    },
+  },
 });
