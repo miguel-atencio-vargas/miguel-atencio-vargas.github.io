70fb83fe5a15eabf21ca3ef109525bcc0911d20c
Miguel Atencio Vargas <atencio947@gmail.com>
2023-09-20 21:31:23 -0400

    Update deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
-});
+  site: 'https://skyfall947.github.io',
+  base: '/portfolio-client-web-astro',
+})
